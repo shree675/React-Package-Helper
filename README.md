f9198bc21901b9ebb9a216831597dfe59369af67
shree675 <shreeteshm37@gmail.com>
2021-08-08 00:29:43 +0530

    generalized version

--- README.md
+++ README.md
@@ -1,25 +1,29 @@
-# React-Package-Helper
+# npm-Package-Helper
 
 ## About
-React Package Helper is a simple Chrome extension that is designed to help users (me, personally) in choosing and browsing different [react](https://reactjs.org/) [nmpjs packages](https://docs.npmjs.com/about-packages-and-modules).
+
+npm Package Helper is a simple Chrome extension that is designed to help users in choosing and browsing different [nmpjs packages](https://docs.npmjs.com/about-packages-and-modules).
 
 ## What it does
-This extension saves your precious time. In case you are unsatisfied with a react package, then navigating back to the search page and browsing for some more relevant packages may be cumbersome and time consuming. Sometimes, you may also miss a few good react packages, in this process, that suit your needs.  
-You may want to save your time while not losing continuity in the hunt for your react package that suits your needs.  
-React Package Helper does exactly that. It comes to your rescue by providing a list of five most relevant react packages other than the one that you are already browsing through. On clicking a particular option of interest, it takes you to the documentation page of that package.
 
-<img src="assets/Screenshot (142).png" height="450">
+This extension saves your precious time. While using any framework, in case you are unsatisfied with an npm package, then navigating back to the search page and browsing for some more relevant packages may be cumbersome and time consuming. Sometimes, you may also miss a few good packages, in this process, that suit your needs.  
+You may want to save your time while not losing continuity in the hunt for your npmjs package that suits your needs.  
+npm Package Helper does exactly that. It comes to your rescue by providing a list of five most relevant packages other than the one that you are already browsing through. On clicking a particular option of interest, it takes you to the documentation page of that package.
 
-***Note: This extension only works in npmjs pages that contain react packages.***
+<img src="assets/Screenshot (336).png" height="450">
+
+**_Note: This extension only works in npmjs web pages that contain react packages._**
 
 ## Demonstration
 
-<img src="assets/React_Packages.gif" height="450">
+<img src="assets/npm.gif" height="450">
 
 This extension has been developed as a personal tool to suit my needs and to save my time while browsing for react packages.
 
 ## Screenshots
+
 For screenshots and videos, visit [assets](assets).
 
 ## Version
+
 v1.0
--- popup.js
+++ popup.js
@@ -1,62 +1,59 @@
-document.addEventListener('DOMContentLoaded', async ()=>{
-    const mainele=document.getElementById("main");
+document.addEventListener("DOMContentLoaded", async () => {
+    const mainele = document.getElementById("main");
     var curUrl;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        console.log(tabs[0].url);
-        curUrl=tabs[0].url;
-        var url=curUrl.substring(0,"https://www.npmjs.com/package/".length);
-        if(url==="https://www.npmjs.com/package/"){
-            const baseUrl="https://api.npms.io/v2/search?q=";    
-            var request=new XMLHttpRequest();
-            try{
-                request.open('GET',baseUrl+curUrl.substring("https://www.npmjs.com/package/".length,200),true);
-                try{
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        curUrl = tabs[0].url;
+        var url = curUrl.substring(0, "https://www.npmjs.com/package/".length);
+        if (url === "https://www.npmjs.com/package/") {
+            const baseUrl = "https://api.npms.io/v2/search?q=";
+            var request = new XMLHttpRequest();
+            try {
+                request.open("GET", baseUrl + curUrl.substring("https://www.npmjs.com/package/".length, 200), true);
+                try {
                     request.send();
+                } catch (error) {
+                    throw "Oops! Some error occurred.\nCheck your internet connection and try again";
                 }
-                catch(error){
-                    throw('Oops! Some error occurred.\nCheck your internet connection and try again');
-                }
-                request.onload = function(){
-                    document.getElementById("cur-package").innerHTML=curUrl.substring("https://www.npmjs.com/package/".length,200);
-                    var data=JSON.parse(request.response);
-                    var packages=data.results;
-                    // console.log(data);
-                    if(packages.length<=5){
-                        for(var i=1;i<=packages.length;i++){
-                            document.getElementsByClassName("name")[i-1].innerHTML=packages[i].package.name;
-                            var cont=packages[i].package.description;
-                            if(cont!==undefined){
-                                document.getElementsByClassName("content")[i-1].innerHTML=packages[i].package.description;                            
+                request.onload = function () {
+                    document.getElementById("cur-package").innerHTML = curUrl.substring("https://www.npmjs.com/package/".length, 200);
+                    var data = JSON.parse(request.response);
+                    var packages = data.results;
+                    if (packages === null || packages === undefined || packages.length === 1) {
+                        document.getElementById("empty").style.display = "block";
+                        return;
+                    }
+                    if (packages.length <= 5) {
+                        for (var i = 1; i < packages.length; i++) {
+                            document.getElementsByClassName("name")[i - 1].innerHTML = packages[i].package.name;
+                            var cont = packages[i].package.description;
+                            if (cont !== undefined) {
+                                document.getElementsByClassName("content")[i - 1].innerHTML = packages[i].package.description;
+                            } else {
+                                document.getElementsByClassName("content")[i - 1].innerHTML = "*No description available*";
                             }
-                            else{
-                                document.getElementsByClassName("content")[i-1].innerHTML="*No description available*";
-                            }                            
-                            document.getElementById("a"+i).href=packages[i].package.links.npm;
+                            document.getElementById("a" + i).style.display = "block";
+                            document.getElementById("a" + i).href = packages[i].package.links.npm;
+                        }
+                    } else {
+                        for (var i = 1; i <= 5; i++) {
+                            document.getElementsByClassName("name")[i - 1].innerHTML = packages[i].package.name;
+                            var cont = packages[i].package.description;
+                            if (cont !== undefined) {
+                                document.getElementsByClassName("content")[i - 1].innerHTML = packages[i].package.description;
+                            } else {
+                                document.getElementsByClassName("content")[i - 1].innerHTML = "*No description available*";
+                            }
+                            document.getElementById("a" + i).style.display = "block";
+                            document.getElementById("a" + i).href = packages[i].package.links.npm;
                         }
                     }
-                    else{
-                        for(var i=1;i<=5;i++){
-                            document.getElementsByClassName("name")[i-1].innerHTML=packages[i].package.name;
-                            var cont=packages[i].package.description;
-                            if(cont!==undefined){
-                                document.getElementsByClassName("content")[i-1].innerHTML=packages[i].package.description;                            
-                            }
-                            else{
-                                document.getElementsByClassName("content")[i-1].innerHTML="*No description available*";
-                            }                            
-                            document.getElementById("a"+i).href=packages[i].package.links.npm;
-                        }
-                    }
-                }        
-            }
-            catch(error){
-                mainele.innerHTML=error;
+                };
+            } catch (error) {
+                mainele.innerHTML = error;
                 console.log(error);
             }
-        }
-        else{
-            mainele.innerHTML="React Packages are not available for this site (cannot detect npmjs-package)";
-            // mainele.innerHTML=curUrl;
+        } else {
+            mainele.innerHTML = "npm packages are not available for this site (cannot detect npmjs-package)";
         }
     });
 });
